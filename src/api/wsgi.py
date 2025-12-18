a3b82b2c57bfa72a6019a708c4744e4d03ca0fb7
Pau <pauventuraa@gmail.com>
2025-12-18 02:02:41 +0000

    Fix wsgi import path

--- src/api/wsgi.py
+++ src/api/wsgi.py
@@ -2,7 +2,7 @@
 # Necesario para herramientas como Gunicorn, uWSGI, y plataformas como Heroku
 
 # Importa la instancia principal de la aplicación Flask desde src/app.py
-from src.app import app as application
+from app import app as application
 
 # Punto de entrada para ejecución directa (desarrollo local)
 if __name__ == "__main__":
